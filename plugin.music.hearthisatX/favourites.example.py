f7b40ff455a957769cbd8d86c03921b4439ec1c9
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-04-30 14:07:52 -0400

    Update favourites.example.py

--- plugin.music.hearthisatX/favourites.example.py
+++ plugin.music.hearthisatX/favourites.example.py
@@ -1,2 +1,2 @@
-## Not functional yet, but a reminder of the syntaxd for adding favourites.
+## Not functional yet, but a reminder of the syntaxdfor adding favourites.
 <favourite name="sara nishino - Retro Disco Vol.3 - 90&apos;s Discotheque" thumb="special://masterprofile/Thumbnails/Video/f/fb7a8c23.tbn">PlayMedia(&quot;https://hearthis.app/saranishino-remix/retro-disco-vol3-90s-discotheque/listen/?s=3Ul&quot;)</favourite>
